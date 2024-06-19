6b7fb6f63376b250f5c4665ca30a74a3fe1a3411
deaxparadox <nitish136900kushwaha@gmail.com>
2024-06-20 01:04:33 +0530

    docker install

--- front/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ChartComponent } from './d3/chart/chart.component';
-
-export const routes: Routes = [
-    {path: 'chart', component: ChartComponent}
-];
